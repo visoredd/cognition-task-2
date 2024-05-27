b60b8d1ac7d5df966785173a5b315e7d87c682d2
Aayush <guptaayush3108@gmail.com>
2024-05-27 17:08:02 +0530

    first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,8 @@
+export const Home = () => {
+  return (
+    <div className="page">
+      <h2>Home page</h2>
+      <p>This is the text for the home page</p>
+    </div>
+  );
+};
---
+++ src/components/structure/Header.js
@@ -0,0 +1,7 @@
+export const RenderHeader = () => {
+  return (
+    <div className="header">
+      <h1>Authentication Task - 2</h1>
+    </div>
+  );
+};
